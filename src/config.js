1456e9472c0429100c9c4fbde0e1b30b5ef0b651
gauravuttarkar <gaurav.uttarkar@gmail.com>
2020-03-31 15:13:45 +0530

    Signup, login, firebase done

--- src/config.js
+++
@@ -1,9 +0,0 @@
-var config = { /* COPY THE ACTUAL CONFIG FROM FIREBASE CONSOLE */
-  apiKey: "AIzaSyCMegaypSpkeDQ4bVa2yipX8pJWGsM3WiI",
-  authDomain: "react-app-885e8.firebaseapp.com",
-  databaseURL: "https://react-app-885e8.firebaseio.com",
-  projectId: "react-app-885e8",
-  storageBucket: "react-app-885e8.appspot.com",
-  messagingSenderId: "807198065368",
-  appId: "1:807198065368:web:d4e19729dbce53ebf98dad"
-};
