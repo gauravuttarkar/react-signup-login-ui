4ddaca6beb085aa43c2241f5bef0618e6077c349
gauravuttarkar <gaurav.uttarkar@gmail.com>
2020-03-31 17:41:56 +0530

    Redirecting to profile page after login

--- src/Login.js
+++ src/Login.js
@@ -1,40 +1,83 @@
 import React from 'react';
 import useSignUpForm from './CustomHooks';
 import fire from './fire';
-
+const createHistory = require("history").createBrowserHistory;
+// import { useHistory }
 
 const Login = () => {
 
   var passwordHash = require('password-hash');
 
 
-  const callbackFunc = () => {
+  async function callbackFunc () {
+
     var email = `${inputs.email}`;
     var userRef = fire.database().ref('users');
     var hashedPassword,firstName,lastName;
-    var query = userRef.orderByChild("email").equalTo(email).on("child_added", function(snapshot) {
+
+    await new Promise(resolve => userRef.orderByChild("email").equalTo(email).on("child_added", function(snapshot) {
       firstName = snapshot.val().firstName;
       lastName = snapshot.val().lastName;
       hashedPassword = snapshot.val().password;
-    });
+      console.log("hi "+snapshot.val().password);
+      var password = `${inputs.password}`;
+      console.log(password)
+      var result = passwordHash.verify(password, hashedPassword);
+      console.log(result);
+      let history = createHistory();
 
-    var password = `${inputs.password}`;
-    var result = passwordHash.verify(password, hashedPassword);
-    console.log(result);
-    // userRef.push ({
-    //    firstName: `${inputs.firstName}`,
-    //    lastName: `${inputs.lastName}` ,
-    //    email:  `${inputs.email}`,
-    //    password: hashedPassword
-    // });
-    if (result){
-    alert(`Successfull login!
-       Email: ${inputs.email}`);
-    }
-    else{
-      alert(`Wrong password`);
-    };
+      // userRef.push ({
+      //    firstName: `${inputs.firstName}`,
+      //    lastName: `${inputs.lastName}` ,
+      //    email:  `${inputs.email}`,
+      //    password: hashedPassword
+      // });
+      if (result){
+      alert(`Successfull login!
+         Email: ${inputs.email}`);
+         history.push("/profile");
+         let pathUrl = window.location.href;
+         window.location.href = pathUrl;
+      }
+      else{
+        alert(`Wrong password`);
+        history.push("/login");
+        let pathUrl = window.location.href;
+        window.location.href = pathUrl;
+      };
+    }));
+
   };
+  // const callbackFunc = () => {
+  //   var email = `${inputs.email}`;
+  //   var userRef = fire.database().ref('users');
+  //   var hashedPassword,firstName,lastName;
+  //   var query = userRef.orderByChild("email").equalTo(email).on("child_added", function(snapshot) {
+  //     firstName = snapshot.val().firstName;
+  //     lastName = snapshot.val().lastName;
+  //     hashedPassword = snapshot.val().password;
+  //     console.log("hi "+snapshot.val().password);
+  //   });
+  //
+  //   var password = `${inputs.password}`;
+  //   console.log(password)
+  //   var result = passwordHash.verify(password, hashedPassword);
+  //   console.log(result);
+  //   // userRef.push ({
+  //   //    firstName: `${inputs.firstName}`,
+  //   //    lastName: `${inputs.lastName}` ,
+  //   //    email:  `${inputs.email}`,
+  //   //    password: hashedPassword
+  //   // });
+  //   if (result){
+  //   alert(`Successfull login!
+  //      Email: ${inputs.email}`);
+  //   }
+  //   else{
+  //     alert(`Wrong password`);
+  //   };
+  // };
+  //
 
 
 
--- src/index.js
+++ src/index.js
@@ -2,14 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Signup from './Signup';
 import Login from './Login'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+
+const Nav = () => (
+  <div>
+    <ul>
+      <li><Link to="/login">Login</Link></li>
+      <li><Link to="/signup">Signup</Link></li>
+    </ul>
+  </div>
+);
+
+const HomePage = () => <h1>Home Page</h1>;
+const ProfilePage = () => <h1>Profile Page</h1>;
+const LoginPage = () => <Login/>;
+const SignupPage = () => <Signup/>
 
 class Page extends React.Component{
+  // render(){
+  //   return(
+  //     <div>
+  //     <Signup/>
+  //     <Login/>
+  //     </div>
+  //   );
+  // }
   render(){
-    return(
-      <div>
-      <Signup/>
-      <Login/>
-      </div>
+  return (
+      <Router>
+
+        <div>
+          <Route exact path="/" component={HomePage} />
+          <Nav />
+          <Route path="/login" component={LoginPage} />
+          <Route path="/signup" component={SignupPage} />
+          <Route path="/profile" component={ProfilePage} />
+        </div>
+      </Router>
     );
   }
 }
