3d56050859a66033cf45bf6f491821b505102d90
gauravuttarkar <gaurav.uttarkar@gmail.com>
2020-04-01 17:07:54 +0530

    Fixed UI for mobile view

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ const HomePage = () => {
     <div>
     <nav className="navbar navbar-expand-lg navbar-light fixed-top">
       <div className="container">
-        <Link className="navbar-brand" to={"/"}>Demo app</Link>
-        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <Link className="navbar-brand collapse" to={"/"}>Demo app</Link>
+
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
               <Link className="nav-link" to={"/login"}>Login</Link>
@@ -24,7 +24,7 @@ const HomePage = () => {
               <Link className="nav-link" to={"/signup"}>Sign up</Link>
             </li>
           </ul>
-        </div>
+
       </div>
     </nav>
     <div class="container">
